bb9cf5865180ffaf08f608588bf67bec1d250432
rahulbhooteshwar <rahulbhooteshwar@users.noreply.github.com>
2020-08-12 08:20:40 +0000

    deploy: 6e34c17e5ebbb92155e28b0d4d791ccef09f0aa7

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rule-engine-demo-client/precache-manifest.bffdb41908c8df07f8dd56af6b2004f8.js"
+  "/rule-engine-demo-client/precache-manifest.5b064da71a1c4b04fa8a3c3b34397684.js"
 );
 
 self.addEventListener('message', (event) => {
